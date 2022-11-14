c891c8fde65593ff0cf9048dae5a17dc91d1a6f2
Aziza Zhalgaspayeva <duisembay12@gmail.com>
2022-11-14 18:29:18 +0600

    Added DB configurations.

---
+++ app/app/tests.py
@@ -0,0 +1,14 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """ Test the calc module. """
+
+    def test_add(self):
+        """ Test adding numbers together. """
+        res = calc.add(100, 10)
+        self.assertEqual(res, 110)
